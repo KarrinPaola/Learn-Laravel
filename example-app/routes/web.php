8f7e3cf6bdad6b0372fabb416555b78240c92cdf
Lê Trung Hiếu <167725687+LeHieu0208@users.noreply.github.com>
2025-05-02 14:36:26 +0000

    controller

--- example-app/routes/web.php
+++ example-app/routes/web.php
@@ -3,9 +3,12 @@
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\UserController;
 use App\Models\User;
+use App\Http\Controllers\CourseController;
 
 Route::get('/', function () {
     return view('welcome');
 });
 
 Route::get("/user/{id}", [UserController::class, "getUser"]);
+Route::get("/course", [CourseController::class,"index"]);
+Route::get("/course/{id}", [CourseController::class,"findCourse"]);
